4e025604e5fcd8b0d65d3428bb62adf1b51292fc
Gapur <110934827+Albogachiev@users.noreply.github.com>
2024-12-13 20:03:14 +0300

    реализовал добавление изображений,и возможность изменять им размер

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -7,6 +7,8 @@ import Table from '@tiptap/extension-table';
 import TableCell from '@tiptap/extension-table-cell';
 import TableHeader from '@tiptap/extension-table-header';
 import TableRow from '@tiptap/extension-table-row';
+import Image from '@tiptap/extension-image';
+import ImageResize from 'tiptap-extension-resize-image';
 
 const Editor = () => {
   const editor = useEditor({
@@ -19,6 +21,8 @@ const Editor = () => {
     },
     extensions: [
       StarterKit,
+      Image,
+      ImageResize,
       Table,
       TableCell,
       TableHeader,
